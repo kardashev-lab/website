59ebf1b9913490c6117d6262c8c161b3e5655a50
AshutoshVJTI <ashutoshmathore@csus.edu>
2025-09-15 18:22:18 -0700

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
@@ -62,6 +63,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.variable}>
+      <Analytics />
       <body className="min-h-screen bg-background font-sans antialiased">
         {children}
       </body>
